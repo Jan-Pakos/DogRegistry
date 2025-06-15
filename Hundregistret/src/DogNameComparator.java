1e2dfaf180a4203866acf1af73defb440877d69f
Jan Pakos <144565382+Jan-Pakos@users.noreply.github.com>
2025-06-15 16:53:31 +0200

    Delete Hundregistret directory

--- Hundregistret/src/DogNameComparator.java
+++
@@ -1,8 +0,0 @@
-// Jan Pakos japa4307
-import java.util.Comparator;
-
-public class DogNameComparator implements Comparator<Dog> {
-    public int compare(Dog nameOne, Dog nameTwo) {
-        return nameOne.getName().compareToIgnoreCase(nameTwo.getName());
-    }
-}
--- Hundregistret/src/DogTailComparator.java
+++
@@ -1,8 +0,0 @@
-// Jan Pakos japa4307
-import java.util.Comparator;
-
-public class DogTailComparator implements Comparator<Dog> {
-    public int compare(Dog tailOne, Dog tailTwo) {
-        return Double.compare(tailOne.getTailLength(), tailTwo.getTailLength());
-    }
-}
